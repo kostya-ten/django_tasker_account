12579f86406a64dcafab30c4131d0a61ec6ed2e8
kostya <kostya@yandex.ru>
2019-04-16 07:24:29 +0300

    travis

--- tasker_account/apps.py
+++ tasker_account/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class TaskerAccountConfig(AppConfig):
     name = 'tasker_account'
-    label = 'tasker_account'
-    verbose_name = 'Python Social'
+    verbose_name = _("Tasker account")
