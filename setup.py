3c9f3f055b085c72f70866734df2ef76f9b9f95c
kostya <kostya@yandex.ru>
2019-04-16 06:18:18 +0300

    travis

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='tasker_account',
+    name='django_tasker_account',
     version='0.01',
     packages=[
         'tests',
