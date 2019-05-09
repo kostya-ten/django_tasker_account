30408246350a8d2b188164f076550db9a00d0547
kostya <kostya@yandex.ru>
2019-05-09 07:06:16 +0300

    context_processors

--- tests/settings.py
+++ tests/settings.py
@@ -72,6 +72,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django_tasker_account.context_processors.settings',
+                'django_tasker_account.context_processors.year',
             ],
         },
     },
