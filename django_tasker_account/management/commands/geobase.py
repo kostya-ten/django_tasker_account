c7a9274957968d6eab9340a5738f0c2fd831ec10
kostya <kostya@yandex.ru>
2019-05-26 00:22:09 +0300

    fix codestyle

--- django_tasker_account/management/commands/geobase.py
+++ django_tasker_account/management/commands/geobase.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django_tasker_account import geobase
 from ipaddress import ip_address
 
