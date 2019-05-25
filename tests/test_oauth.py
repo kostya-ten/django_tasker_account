48392d09d9222f85c80a7e850d73fe613d3ae9e5
kostya <kostya@yandex.ru>
2019-05-25 23:11:04 +0300

    test reverse url

--- tests/test_oauth.py
+++ tests/test_oauth.py
@@ -19,27 +19,27 @@ class OAuth(TestCase, test_base.Request):
     def test_view(self):
         factory = RequestFactory(HTTP_HOST='localhost')
 
-        request = factory.get('/accounts/oauth/google/')
+        request = factory.get(reverse('django_tasker_account:oauth_google'))
         response = views.oauth_google(request)
         self.assertRegex(response.url, r'^https://accounts.google.com/o/oauth2/v2/auth')
         self.assertEqual(response.status_code, 302)
 
-        request = factory.get('/accounts/oauth/yandex/')
+        request = factory.get(reverse('django_tasker_account:oauth_yandex'))
         response = views.oauth_yandex(request)
         self.assertRegex(response.url, r'^https://oauth.yandex.ru/authorize')
         self.assertEqual(response.status_code, 302)
 
-        request = factory.get('/accounts/oauth/mailru/')
+        request = factory.get(reverse('django_tasker_account:oauth_mailru'))
         response = views.oauth_mailru(request)
         self.assertRegex(response.url, r'^https://oauth.mail.ru/login')
         self.assertEqual(response.status_code, 302)
 
-        request = factory.get('/accounts/oauth/vk/')
+        request = factory.get(reverse('django_tasker_account:oauth_vk'))
         response = views.oauth_vk(request)
         self.assertRegex(response.url, r'^https:///oauth.vk.com/authorize')
         self.assertEqual(response.status_code, 302)
 
-        request = factory.get('/accounts/oauth/facebook/')
+        request = factory.get(reverse('django_tasker_account:oauth_facebook'))
         response = views.oauth_facebook(request)
         self.assertRegex(response.url, r'^https://www.facebook.com/v3.2/dialog/oauth')
         self.assertEqual(response.status_code, 302)
